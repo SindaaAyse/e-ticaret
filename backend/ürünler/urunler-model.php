41af3dce98d97cdbbd635617d89d96b59c417640
sindaakyil <73520969+sindaakyil@users.noreply.github.com>
2022-07-09 00:24:58 +0300

    urunler-model.php

---
+++ backend/ürünler/urunler-model.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class urunler extends Model
+{
+
+    public $timestamps = false;
+
+    protected $table = "urunler";
+
+    protected $fillable = [
+        "id",
+        "kat_id",
+        "kodu",
+        "adi",
+        "slug",
+        "resim",
+        "fiyat",
+        "kdv",
+        "aciklama",
+        "anasayfa",
+        "silindi",
+    ];
+
+
+}
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class urunler extends Model
+{
+
+    public $timestamps = false;
+
+    protected $table = "urunler";
+
+    protected $fillable = [
+        "id",
+        "kat_id",
+        "kodu",
+        "adi",
+        "slug",
+        "resim",
+        "fiyat",
+        "kdv",
+        "aciklama",
+        "anasayfa",
+        "silindi",
+    ];
+
+
+}
