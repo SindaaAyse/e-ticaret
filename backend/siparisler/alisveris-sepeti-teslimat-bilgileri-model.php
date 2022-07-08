3a60d9e628bc5398e19ad4665651bac5573e6e27
Ayşe <Ayseekinci.02.00@gmail.com>
2022-07-09 01:40:30 +0300

    Create alisveris-sepeti-teslimat-bilgileri-model.php

---
+++ backend/siparisler/alisveris-sepeti-teslimat-bilgileri-model.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class alisverisSepetiTeslimat extends Model
+{
+
+    public $timestamps = false;
+
+    protected $table = "alisveris_sepeti_teslimat";
+
+    protected $fillable = [
+        "id",
+        "sepet_id",
+        "ad_soyad",
+        "gsm",
+        "il",
+        "ilce",
+        "adres",
+        "tarih",
+    ];
+
+
+}
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class alisverisSepetiTeslimat extends Model
+{
+
+    public $timestamps = false;
+
+    protected $table = "alisveris_sepeti_teslimat";
+
+    protected $fillable = [
+        "id",
+        "sepet_id",
+        "ad_soyad",
+        "gsm",
+        "il",
+        "ilce",
+        "adres",
+        "tarih",
+    ];
+
+
+}
