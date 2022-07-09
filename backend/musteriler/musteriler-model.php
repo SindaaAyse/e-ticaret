610258b783f5f82c0e2904ee318d608eb343430e
Ayşe <Ayseekinci.02.00@gmail.com>
2022-07-09 08:44:40 +0300

    Create musteriler-model.php

---
+++ backend/musteriler/musteriler-model.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class musteriler extends Model
+{
+
+    public $timestamps = false;
+
+    protected $table = "musteriler";
+
+    protected $fillable = [
+        "id",
+        "ad_soyad",
+        "email",
+        "sifre",
+        "gsm",
+        "il",
+        "ilce",
+        "adres",
+        "tarih",
+    ];
+
+
+}
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class musteriler extends Model
+{
+
+    public $timestamps = false;
+
+    protected $table = "musteriler";
+
+    protected $fillable = [
+        "id",
+        "ad_soyad",
+        "email",
+        "sifre",
+        "gsm",
+        "il",
+        "ilce",
+        "adres",
+        "tarih",
+    ];
+
+
+}
